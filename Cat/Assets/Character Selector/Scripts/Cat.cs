8b89b62c115f4f1593e241a2f0aa1704c8763fea
Himajak95 <47122804+Himajak95@users.noreply.github.com>
2019-04-15 12:56:47 -0500

    Added Unity  Project

---
+++ Cat/Assets/Character Selector/Scripts/Cat.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cat: MonoBehaviour
+{
+    public Material[] furColor;
+    public Material[] eyeColor;
+    public GameObject body;
+    public GameObject rightEye;
+    public GameObject leftEye;
+    public int black = 0, bdbrown = 0, bdgray = 0,buf=0;
+   // public Text msg;
+
+    Renderer rend;
+
+     void Start()
+    {
+       // msg.text = " ";
+        int colorIndex = (int)(Random.value * 3);
+        RenderCube(body, colorIndex, eyeColor);
+        buf++;
+        if (colorIndex == bdbrown)
+            bdbrown++;
+        else if (colorIndex == bdgray)
+            bdgray++;
+
+
+        colorIndex = (int)(Random.value * 3);
+        RenderCube(rightEye, colorIndex, eyeColor);
+        RenderCube(leftEye, colorIndex, eyeColor);
+
+        if (colorIndex == bdbrown)
+            bdbrown++;
+        else if (colorIndex == bdgray)
+            bdgray++;
+
+       /* if (buf < 7)
+            if (bdgray > bdbrown)
+                msg.text = "Fight";            
+           // else if()*/
+    }
+
+    private void RenderCube(GameObject obj, int colorIndex, Material[] color)
+    {
+        rend = obj.GetComponent<Renderer>();
+        rend.enabled = true;
+
+        rend.sharedMaterial = color[colorIndex];
+    }
+
+}
---
+++ Cat/Assets/Character Selector/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject MyCat;
+    public static int buf = 0;
+
+    public void Spawn(Vector3 position)
+    {
+        buf++;
+        Instantiate(MyCat).transform.position = position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+           
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, MyCat.transform.position.z);
+            if(buf<7)
+            Spawn(adjustZ);
+        }
+    }
+}
